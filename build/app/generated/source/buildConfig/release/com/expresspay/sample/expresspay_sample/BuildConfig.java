c4e3d48b4c85b4fc67b747a4408eb04b998fa3ef
zik <a2zzuhaib@gmail.com>
2024-01-29 19:40:55 +0300

    commit

---
+++ build/app/generated/source/buildConfig/release/com/expresspay/sample/expresspay_sample/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.expresspay.sample.expresspay_sample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.expresspay.sample";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 3;
+  public static final String VERSION_NAME = "1.0.0";
+}
---
+++ build/expresspay_sdk/generated/source/buildConfig/release/com/expresspay/fluttersdk/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.expresspay.fluttersdk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.expresspay.fluttersdk";
+  public static final String BUILD_TYPE = "release";
+}
